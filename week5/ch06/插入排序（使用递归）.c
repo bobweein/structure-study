c9e7e1c7369245d2a28888e1aab5e1f70e111ead
bobweein <91543576+bobweein@users.noreply.github.com>
2021-11-13 21:56:38 +0800

    Update 插入排序（使用递归）.c

--- week5/ch06/插入排序（使用递归）.c
+++ week5/ch06/插入排序（使用递归）.c
@@ -2,22 +2,22 @@
 void insertionsort(int arr[], int n)
 {
 	void Printarr(int arr[], int n);
-	if (n != 1)
+	if (n != 1)//递归结束的标志
 		insertionsort(arr, n - 1);
 	int temp,i;
-	temp = arr[n];
+	temp = arr[n];//当前递归中数组的最后一个（不是最大的那个数组的最后一个）
 	i = n - 1;
 	while(i>=0)
 	{
-		if (arr[i] > temp)
+		if (arr[i] > temp)//如果arr[i]大于temp;将arr[i]赋给arr[i+1]，相当于将他向后移一个位置！
 		{
-			arr[i + 1] = arr[i];//3,2,4,10,1,2,5,6,12,13,30,8 
-			if (i ==0) 
+			arr[i + 1] = arr[i];
+			if (i ==0) //当arr[0]被往后移一个一个位置时，将temp放在数组的最前端！
 			{
 				arr[i] = temp; break;
 			}
 		}
-		if (arr[i] <=temp)
+		if (arr[i] <=temp)//如果arr[i]<=temp时，将temp放在arr[i]的后一个位置，即arr[i+1]!
 		{
 			arr[i + 1] = temp;
 			break;
