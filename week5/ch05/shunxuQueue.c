70b5d04b883d809ff6a03a984dc62c6dff3c9351
bobweein <91543576+bobweein@users.noreply.github.com>
2021-11-13 20:36:38 +0800

    Update shunxuQueue.c

--- week5/ch05/shunxuQueue.c
+++ week5/ch05/shunxuQueue.c
@@ -2,47 +2,50 @@
 #define QueueSize 5
 struct Queue
 {
-	int AQueue[QueueSize];
-	int front;
-	int rear;
-	int flag;
+	int AQueue[QueueSize];//设置队列数组
+	int front;//队头指针
+	int rear;//队尾指针
+	int flag;//队满对空的标志,元素入队成功时为1，出队成功时为0
 };
 typedef struct Queue SCQ, * SCQNext;
+//初始化
 void Init(SCQNext Q)
 {
 	Q->front = Q->rear=0;
-}
+	Q->flag=0;
+}//队列是否为空
 int IsEmpty(SCQNext Q)
 {
-	if (Q->front == Q->rear && Q->flag == 0)
+	if (Q->front == Q->rear && Q->flag == 0)//队空
 		return 1;
 	else return 0;
 }
+//入队
 int RuiQueue(SCQNext Q,int x)
 {
-	if (Q->front == Q->rear && Q->flag == 1)
+	if (Q->front == Q->rear && Q->flag == 1)//堆满的判断标准
 		return 0;
 	else
 	{
 		if (x == 0) return 0;
 		Q->AQueue[Q->rear] = x;
-		Q->rear = (Q->rear + 1) % QueueSize;
-		Q->flag = 1;
+		Q->rear = (Q->rear + 1) % QueueSize;//使数组在逻辑上是循环的，并且当队列没满时，指向空位
+		Q->flag = 1;//表示元素入队成功
 		return 1;
 	}
-}
+}//元素出队
 int ChuQueue(SCQNext Q, int* e)
 {
-	if (Q->front == Q->rear && Q->flag == 0)
+	if (Q->front == Q->rear && Q->flag == 0)//队空的判断标准
 		return 0;
 	else
 	{
-		*e = Q->AQueue[Q->front];
+		*e = Q->AQueue[Q->front];//把要出队的元素赋给指针变量
 		Q->front= (Q->front + 1) % QueueSize;
-		Q->flag = 0;
+		Q->flag = 0;//元素出队成功
 		return 1;
 	}
-}
+}//打印队列
 void PrintQueue(SCQNext Q)
 {
 	if (Q->front == Q->rear && Q->flag == 0)
@@ -59,7 +62,7 @@ void PrintQueue(SCQNext Q)
 		 printf("\n");
 		 return 1;
 	}
-}
+}//队列长度
 int LenQueue(SCQNext Q)
 {
 	int n = 0;
